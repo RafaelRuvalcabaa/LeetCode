d888b418970d724cd3cfb90cf1689453f46c0eb8
RafaelRuvalcaba <157087378+RafaelRuvalcabaa@users.noreply.github.com>
2026-05-13 22:37:12 -0600

    add: some codes that i made in leetcode

---
+++ Binary Search/first_bad.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        left = 1 
+        right = n
+        temp = 0
+        while left <= right: 
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                temp = mid
+                right = mid - 1 
+            else: 
+                left = mid + 1
+        return temp 
