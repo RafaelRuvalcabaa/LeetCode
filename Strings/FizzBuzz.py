80b86b24243ccdacf613043093d12f761c19a6d7
RafaelRuvalcaba <157087378+RafaelRuvalcabaa@users.noreply.github.com>
2026-06-09 21:06:31 -0600

    ya hice fizzbuzz y running sum, al fin le entendi al acumulador :D

---
+++ Strings/FizzBuzz.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        resultado = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                resultado.append("FizzBuzz")
+            elif i % 3 == 0:
+                resultado.append("Fizz")
+            elif i % 5 == 0:
+                resultado.append("Buzz")
+            else:
+                resultado.append(str(i))
+        return resultado
